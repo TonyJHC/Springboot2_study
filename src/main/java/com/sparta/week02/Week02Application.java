cb212b7f1b23af20afeabd9a843d50dbdd522e96
TonyJHC <jongvin01@naver.com>
2021-08-22 18:04:31 +0900

    week02

---
+++ src/main/java/com/sparta/week02/Week02Application.java
@@ -0,0 +1,53 @@
+package com.sparta.week02;
+
+import com.sparta.week02.domain.Course;
+import com.sparta.week02.domain.CourseRepository;
+import com.sparta.week02.domain.CourseRequestDto;
+import com.sparta.week02.service.CourseService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.List;
+
+
+@EnableJpaAuditing // 생성일자/수정일자 반영을 위해서
+@SpringBootApplication
+public class Week02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week02Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
+        return (args) -> {
+            courseRepository.save(new Course("Fever", "Choi"));
+
+            System.out.println("data print");
+            List<Course> courseList = courseRepository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+
+
+//            Course new_course = new Course("Spring", "Tony"); // 안좋은 예시임 Dto를 만들어서 해야됨
+            CourseRequestDto requestDto = new CourseRequestDto("Petcoms", "Hyun"); // Dto만들어서 함
+            courseService.update(1L, requestDto);
+            courseList = courseRepository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+
+
+        };
+    }
+}
---
+++ src/main/java/com/sparta/week02/domain/CourseRequestDto.java
@@ -0,0 +1,13 @@
+package com.sparta.week02.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor  // 2. 단 ! 해당 어노테이션 필요함
+public class CourseRequestDto {
+    private final String title; // 1. final로 필수적인 요소라고 명함 --> 필요한 생성자를 자동으로 만들어줌
+    private final String tutor;
+}
