1da32e9d2ccd60978fa35c06079117238f86de04
TonyJHC <jongvin01@naver.com>
2021-08-24 00:27:18 +0900

    CourseApi

--- src/main/java/com/sparta/week02/Week02Application.java
+++ src/main/java/com/sparta/week02/Week02Application.java
@@ -13,7 +13,47 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import java.util.List;
 
 
-@EnableJpaAuditing // 생성일자/수정일자 반영을 위해서
+//@EnableJpaAuditing // 생성일자/수정일자 반영을 위해서
+//@SpringBootApplication
+//public class Week02Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Week02Application.class, args);
+//    }
+//
+//    @Bean
+//    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
+//        return (args) -> {
+//            courseRepository.save(new Course("Fever", "Choi"));
+
+//            System.out.println("data print");
+//            List<Course> courseList = courseRepository.findAll();
+//            for (int i = 0; i < courseList.size(); i++) {
+//                Course course = courseList.get(i);
+//                System.out.println(course.getId());
+//                System.out.println(course.getTitle());
+//                System.out.println(course.getTutor());
+//            }
+//
+//
+//            Course new_course = new Course("Spring", "Tony"); // 안좋은 예시임 Dto를 만들어서 해야됨
+//            CourseRequestDto requestDto = new CourseRequestDto("Petcoms", "Hyun"); // Dto만들어서 함
+//            courseService.update(1L, requestDto);
+//            courseList = courseRepository.findAll();
+//            for (int i = 0; i < courseList.size(); i++) {
+//                Course course = courseList.get(i);
+//                System.out.println(course.getId());
+//                System.out.println(course.getTitle());
+//                System.out.println(course.getTutor());
+//            }
+//
+
+//        };
+//    }
+//}
+
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class Week02Application {
 
@@ -24,30 +64,26 @@ public class Week02Application {
     @Bean
     public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
         return (args) -> {
-            courseRepository.save(new Course("Fever", "Choi"));
+            courseRepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
 
-            System.out.println("data print");
+            System.out.println("데이터 인쇄");
             List<Course> courseList = courseRepository.findAll();
-            for (int i = 0; i < courseList.size(); i++) {
+            for (int i=0; i<courseList.size(); i++) {
                 Course course = courseList.get(i);
                 System.out.println(course.getId());
                 System.out.println(course.getTitle());
                 System.out.println(course.getTutor());
             }
 
-
-//            Course new_course = new Course("Spring", "Tony"); // 안좋은 예시임 Dto를 만들어서 해야됨
-            CourseRequestDto requestDto = new CourseRequestDto("Petcoms", "Hyun"); // Dto만들어서 함
+            CourseRequestDto requestDto = new CourseRequestDto("웹개발의 봄, Spring", "임민영");
             courseService.update(1L, requestDto);
             courseList = courseRepository.findAll();
-            for (int i = 0; i < courseList.size(); i++) {
+            for (int i=0; i<courseList.size(); i++) {
                 Course course = courseList.get(i);
                 System.out.println(course.getId());
                 System.out.println(course.getTitle());
                 System.out.println(course.getTutor());
             }
-
-
         };
     }
 }
